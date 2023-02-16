30e84aae97686679a0e4b83795ae7d3691c933d3
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-16 10:05:30 -0600

    added api call

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-import { Navbar } from "./";
+import React, { useState, useEffect } from "react";
+import { Navbar, Content } from "./";
 
 const Main = () => {
+    const [puppies, setPuppies] = useState([]);
+
+    const getPuppies = async () => {
+        try {
+            const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2301-ftb-et-web-et/players');
+            const data = await response.json();
+
+            console.log(data.data.players)
+            setPuppies(data.data.players)
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        getPuppies();
+    },
+    []);
+
     return(
         <div id="main">
             <Navbar />
+            <Content />
         </div>
     )
 }
