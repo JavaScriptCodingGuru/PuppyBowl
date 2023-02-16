e34aff0e28d6f06cc154717e7e9fd3bcad112361
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-16 10:54:51 -0600

    added CSS and updated styiling

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Content = (props) => {
-const puppies = props.puppies;
+  const puppies = props.puppies;
 
-return <div className="puppy-list">
-{
-    puppies.map((puppy, idx)=>
-    {
-        return <div key={puppy.id} id={puppy.id} className = "puppy-box">
+  return (
+    <div className="puppy-list">
+      {puppies.map((puppy, idx) => {
+        return (
+          <div key={puppy.id} id={puppy.id} className="puppy-box">
             <div className="puppy-box-header">
-            <p>{puppy.name}</p>
-            <p>{puppy.id}</p>
+              <h2>{puppy.name}</h2>
+              <h3>#{puppy.id}</h3>
             </div>
-            <img src={puppy.imageUrl}/>
-            <button >See Details</button>
+            <img src={puppy.imageUrl} />
+            <button>See Details</button>
             <button>Delete from Roster</button>
-        </div>
-    })
-}
-
-</div>
-
-
-}
-
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Content;
